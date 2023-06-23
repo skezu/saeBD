1bd40f55664b1d1ea8671abc7e318e452022b1b5
skezu <skez.test@gmail.com>
2023-06-23 09:37:50 +0200

    + fix requetes.sql

--- requetes.sql
+++ requetes.sql
@@ -1,51 +1,51 @@
 -- 4. Quelle classe de Monstre le héros Genos a-t-il combattu ?
-SELECT R2.nomRang from Rang
-INNER JOIN Rang R2 ON R2.numRang = R.numRang
-INNER JOIN Personnage P1 ON P1.Personnage.numRang = R.numRang
-INNER JOIN Personnage P2 ON P2.Personnage.numRang = R2.numRang
-INNER JOIN rolerole R ON R.numrolerole = P2.numrolerole
-INNER JOIN A_FAIT A ON P1.numPersonnage = A.numPersonnage
-INNER JOIN A_FAIT A2 ON P2.numPersonnage = A2.numPersonnage
+SELECT R2.nomRang from Rang R
+INNER JOIN Rang R2 ON R2.NUMRANG = R.numRang
+INNER JOIN PERSONNAGE P1 ON P1.RANGPERSONNAGE = R.numRang
+INNER JOIN Personnage P2 ON P2.RANGPERSONNAGE = R2.numRang
+INNER JOIN ROLEROLE R ON R.NUMROLEROLE = P2.ROLEPERSONNAGE
+INNER JOIN EVENEMENTPERSONNAGE A ON P1.numPersonnage = A.numPersonnage
+INNER JOIN EVENEMENTPERSONNAGE A2 ON P2.numPersonnage = A2.numPersonnage
 WHERE A.numEvenement=A2.numEvenement
 AND P1.nomPersonnage = 'Genos'
-AND P2.nomrolerole = 'Monstre';
+AND P2.ROLEPERSONNAGE = 'Monstre';
 
 
 -- 5. Quelle ville a sollicité le plus d’intervention de héros
 Select numVille, nomVille, count(numEvenement) from Ville
-INNER JOIN Lieu L ON L.numVille = Ville.numVille
-INNER JOIN Evenement E ON E.numEvenement = L.numEvenement
-INNER JOIN A_FAIT A ON A.numEvenement = E.numEvenement
+INNER JOIN Lieu L ON L.VILLELIEU = Ville.numVille
+INNER JOIN Evenement E ON E.LIEUEVENEMENT = L.NUMLIEU
+INNER JOIN EVENEMENTPERSONNAGE A ON A.numEvenement = E.numEvenement
 INNER JOIN Personnage P ON P.numPersonnage = A.numPersonnage
-INNER JOIN rolerole R ON R.numrolerole = P.numrolerole
-WHERE R.nomrolerole = 'Héros'
+INNER JOIN ROLEROLE R ON R.NUMROLEROLE = P.ROLEPERSONNAGE
+WHERE R.NOMROLEROLE = 'Héros'
 GROUP BY numVille, nomVille
 HAVING count(numEvenement) = (SELECT max(count(numEvenement)) from Ville
-INNER JOIN Lieu L ON L.numVille = Ville.numVille
-INNER JOIN Evenement E ON E.numEvenement = L.numEvenement
-INNER JOIN A_FAIT A ON A.numEvenement = E.numEvenement
+INNER JOIN Lieu L ON L.VILLELIEU = Ville.numVille
+INNER JOIN Evenement E ON E.LIEUEVENEMENT = L.NUMLIEU
+INNER JOIN EVENEMENTPERSONNAGE A ON A.numEvenement = E.numEvenement
 INNER JOIN Personnage P ON P.numPersonnage = A.numPersonnage
-INNER JOIN rolerole R ON R.numrolerole = P.numrolerole
-WHERE R.nomrolerole = 'Héros');
+INNER JOIN ROLEROLE R ON R.numRoleRole = P.ROLEPERSONNAGE
+WHERE R.NOMROLEROLE = 'Héros');
 
 -- 6. Donner la liste nominative des amis de l'héro Saitama
 SELECT nomPersonnage, AliasPersonnage
 FROM Personnage P1
 INNER JOIN Personnage P2 ON P1.numPersonnage = P2.numPersonnage
-INNER JOIN EST_EN_RELATION_AVEC R ON P1.numPersonnage = R.numPersonnage
-INNER JOIN EST_EN_RELATION_AVEC R2 ON P2.numPersonnage = R2.numPersonnage
+INNER JOIN RELATION R ON P1.numPersonnage = R.numPersonnage
+INNER JOIN RELATION R2 ON P2.numPersonnage = R2.numPersonnage
 WHERE R.typeRelation='Ami' AND P2.nomPersonnage = 'Saitama';
 
 -- 7. Donner la liste des monstres qui ont battu plus de 15 héros
-SELECT P1.numPersonnage, P1.nomPersonnage, P1.AliasPersonnage, P2.numPersonnage
+SELECT P1.NUMPERSONNAGE, P1.nomPersonnage, P1.AliasPersonnage, P2.numPersonnage
 FROM Personnage P1
 INNER JOIN Personnage P2 ON P1.numPersonnage = P2.numPersonnage
-INNER JOIN rolerole R ON R.numrolerole = P1.numrolerole
-INNER JOIN rolerole R2 ON R2.numrolerole = P2.numrolerole
-INNER JOIN A_FAIT AF ON P1.numPersonnage = AF.numPersonnage
-INNER JOIN A_FAIT AF2 ON P2.numPersonnage = AF2.numPersonnage
-WHERE R.nomrolerole = 'Monstre' AND R2.nomrolerole = 'Héros'
-GROUP BY nomPersonnage, AliasPersonnage
+INNER JOIN RoleRole R ON R.numRoleRole = P1.ROLEPERSONNAGE
+INNER JOIN RoleRole R2 ON R2.NUMROLEROLE = P2.ROLEPERSONNAGE
+INNER JOIN EVENEMENTPERSONNAGE AF ON P1.numPersonnage = AF.numPersonnage
+INNER JOIN EVENEMENTPERSONNAGE AF2 ON P2.numPersonnage = AF2.numPersonnage
+WHERE R.nomRoleRole = 'Monstre' AND R2.nomRoleRole = 'Héros'
+GROUP BY numpersonnage, nomPersonnage, AliasPersonnage
 HAVING count(P1.numPersonnage)>=15;
 
 -- 8. Donner la liste des combats d’un épisode X
@@ -62,8 +62,8 @@ end;
 CREATE OR REPLACE FUNCTION nbPersonnageParEpisode(numEpisode INT) RETURN INTEGER IS
 nbPersonnage INTEGER;
 BEGIN
-    SELECT COUNT(*) INTO nbPersonnage FROM APPARAIT
-    WHERE APPARAIT.numEpisode = numEpisode;
+    SELECT COUNT(*) INTO nbPersonnage FROM EPISODEPERSONNAGE
+    WHERE EPISODEPERSONNAGE.numEpisode = numEpisode;
     RETURN nbPersonnage;
 end;
 -- 10. Donner les vainqueurs des différents combats à partir d’un épisode X avec leurs adversaires
@@ -72,9 +72,9 @@ end;
 SELECT nomPersonnage, AliasPersonnage
 FROM Personnage P1
  INNER JOIN Personnage P2 ON P1.numPersonnage = P2.numPersonnage
- INNER JOIN EST_EN_RELATION_AVEC R ON P1.numPersonnage = R.numPersonnage
- INNER JOIN EST_EN_RELATION_AVEC R2 ON P2.numPersonnage = R2.numPersonnage
-WHERE R.typeRelation='Ami' AND P2.nomPersonnage = 'Saitama' AND P1.sexePersonnage = 'M';
+ INNER JOIN RELATION R ON P1.numPersonnage = R.numPersonnage
+ INNER JOIN RELATION R2 ON P2.numPersonnage = R2.numPersonnage
+WHERE R.typeRelation='Ami' AND P2.nomPersonnage = 'Saitama' AND P1.SEXPERSONNAGE = 'M';
 
 -- 12. Donner la description de l’épisode précédent l’épisode X
 CREATE OR REPLACE PROCEDURE descrPrec1(numDeEpisode NUMBER) 
@@ -88,6 +88,17 @@ BEGIN
 	DBMS_OUTPUT.PUT_LINE(descr);
 
 END;
+
+-- triggers
+CREATE OR REPLACE TRIGGER VerifierAgePersonnage
+    BEFORE INSERT ON Personnage
+    FOR EACH ROW
+BEGIN
+    IF :NEW.agePersonnage < 18 THEN
+        RAISE_APPLICATION_ERROR(-20001, 'L''âge du personnage doit être supérieur ou égal à 18 ans.');
+    END IF;
+END;
+/
 
 -- 13. Donner le nombre d’épisodes de la saison X
 CREATE OR REPLACE PROCEDURE nbEpisode(numDeSaison NUMBER)
