eb46720dc31a3cf95599bbb5ab503c018ed5883d
skezu <skez.test@gmail.com>
2023-06-23 09:40:16 +0200

    + fix requetes.sql + ajout de vues.sql

--- vues.sql
+++ vues.sql
@@ -1,49 +1,30 @@
---### Vue "VuePersonnageSpecialite" :
-
---Cette vue affiche les informations sur les personnages ainsi que leurs spécialités.
-
+---------Vue "VuePersonnageSpecialite" :
 CREATE VIEW VuePersonnageSpecialite AS
 SELECT p.numPersonnage, p.nomPersonnage, p.aliasPersonnage, s.nomSpecialite
 FROM Personnage p
-         JOIN SpecialitePersonnage sp ON p.numPersonnage = sp.numPersonnage
-         JOIN Specialite s ON sp.numSpecialite = s.numSpecialite;
+JOIN SpecialitePersonnage sp ON p.numPersonnage = sp.numPersonnage
+JOIN Specialite s ON sp.numSpecialite = s.numSpecialite;
 
-
---### Vue "VueEvenementLieu" :
-
---Cette vue affiche les événements avec leurs lieux correspondants.
-
+---------Vue "VueEvenementLieu" :
 CREATE VIEW VueEvenementLieu AS
 SELECT e.numEvenement, e.descriptionEvenement, ep.nomEpisode, l.nomLieu
 FROM Evenement e
-         JOIN Episode ep ON e.episodeEvenement = ep.numEpisode
-         JOIN Lieu l ON e.LieuEvenement = l.numLieu;
-
-
---### Vue "VuePersonnageEpisode" :
-
---Cette vue affiche les personnages présents dans chaque épisode.
-
+JOIN Episode ep ON e.episodeEvenement = ep.numEpisode
+JOIN Lieu l ON e.LieuEvenement = l.numLieu;
+-------Vue "VuePersonnageEpisode" :
 CREATE VIEW VuePersonnageEpisode AS
-SELECT ep.numEpisode, p.nomPersonnage
+SELECT ep.numEpisode, e.nomEpisode, p.nomPersonnage
 FROM EpisodePersonnage ep
-         JOIN Personnage p ON ep.numPersonnage = p.numPersonnage;
-
-
---### Vue "VueArcSaison" :
-
---Cette vue affiche les arcs avec les saisons auxquelles ils appartiennent.
-
+JOIN Episode e ON ep.numEpisode = e.numEpisode
+JOIN Personnage p ON ep.numPersonnage = p.numPersonnage;
+--------Vue "VueArcSaison" :
 CREATE VIEW VueArcSaison AS
 SELECT a.numArc, a.nomArc, s.nomSaison
 FROM Arc a
-         JOIN Saison s ON a.numArc = s.Arc;
+JOIN Saison s ON a.numArc = s.Arc;
 
---### Vue "VueClanPersonnage" :
-
---Cette vue affiche les clans avec les personnages qui en font partie.
-
+---------Vue "VueClanPersonnage" :
 CREATE VIEW VueClanPersonnage AS
 SELECT c.numClan, c.nomClan, p.nomPersonnage
 FROM Clan c
-         JOIN Personnage p ON c.numClan = p.Clan;
+JOIN Personnage p ON c.numClan = p.Clan;
